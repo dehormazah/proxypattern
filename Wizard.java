3565b52264267554b06f026518776cd4067060b3
dehormazah <dehormazah@unal.edu.co>
2018-05-21 15:46:40 -0500

    Add files via upload

---
+++ Wizard.java
@@ -0,0 +1,17 @@
+
+package proxy;
+
+
+public class Wizard {
+    
+  private final String name;
+
+  public Wizard(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
---
+++ WizardTower.java
@@ -0,0 +1,8 @@
+
+package proxy;
+
+
+public interface WizardTower {
+     void enter(Wizard wizard);
+    
+}
